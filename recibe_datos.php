794d9f6b2e9357e7accd1dfa5187041b8ffa0c4f
oscarlmj <oscarlmorenojorge@gmail.com>
2023-10-10 17:24:26 +0100

    Inserción del codigo PHP que recibe los datos y realiza las comprobaciones y muestra por pantalla los resultados

--- recibe_datos.php
+++ recibe_datos.php
@@ -1,3 +1,43 @@
 <?php
     include "funciones_validacion.php";
+
+    $nombre_formulario=$_POST["nombre_formulario"];
+    $mensaje = "";
+
+    switch($nombre_formulario){
+        case "Oscar":
+            $validacion= valida_nombre($_POST["nombre"]) && valida_tlf($_POST["telefono"])  && valida_direccion($_POST["direccion"]);
+            break;
+        case "Enrique":
+            $validacion= valida_nombre($_POST["nombre"]);
+            break;
+        case "Victor":
+            $validacion= valida_nombre($_POST["nombre"]);
+            break;
+        default:
+            $mensaje="<h2>Error al recibir los datos del formulario</h2>";
+    }
+
+    if($validacion)
+    {
+        $mensaje ="<h2>Datos recibidos y validados correctamente</h2>";
+        foreach ($_POST as $clave => $valor)
+        {
+            if ($clave !== "nombre_formulario")
+
+                // Invocamos a htmlspecialchars para evitar incluir código HTML o Javascript en la página de respuesta
+                echo "<br> - $clave: ". htmlspecialchars($valor);
+        }
+    }
+    else
+    {
+        $mensaje = "<h2>Error al validar los datos</h2>";
+    }
+
 ?>
+
+<html>
+    <body>
+        <?php echo $mensaje; ?>
+    </body>
+</html>
